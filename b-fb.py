725c8f05c086b566342af73fd210d21a5e961673
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-01-31 21:33:05 +0700

    Add files via upload

--- b-fb.py
+++ b-fb.py
@@ -310,7 +310,7 @@ def login():
 	loading()
 	banner()
 	prints(nel(f'              {P2}INFO AUTHOR{P2}',width=70,padding=(0,7),style=f"{color_panel}"))
-    prints(nel(f"""Author: Khoirul-Xd   Wa: 081283547452  Status: Premium    Github: https://github.com/khoirulez""",width=70,padding=(0,7),style=f"{color_panel}"))
+    prints(nel(f'Author: Khoirul-Xd   Wa: 081283547452  Status: Premium    Github: https://github.com/khoirulez",width=70,padding=(0,7),style=f"{color_panel}"))
     prints(nel(f'              {P2}INFO USER{P2}',width=70,padding=(0,7),style=f"{color_panel}"))  
     prints(nel(f"""{P2}[{color_text}("Your Ip:", data['ip']){P2}]    [{color_text}("Lokasi:", data['loc']){P2}]
                   [{color_text}("Region:", data['region']){P2}]    [{color_text}("Kota:", data['city']){P2}]""",width=70,padding=(0,7),style=f"{color_panel}"))
