83dfdf52494e0773cc3b62dfc7a27d86ff5507dd
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-01 21:56:32 +0700

    Add files via upload

--- b-fb.py
+++ b-fb.py
@@ -342,13 +342,13 @@ def login():
 	loading()
 	banner()
 	waktu()
-	print(nel("[green]Author: [purple]Khoirul-Xd\n"
-        "[green]Status: [red]Premium\n"
-        "[green]GitHub: [blue]https://github.com/khoirulez\n"width=70, padding=(0, 7), style=f"{color_panel}"))
-    print(nel(f"[bold cyan]Your IP: [bold green]{cek_data}\n"
-        f"[bold cyan]Region: [bold green]{asal_reg}\n"
-        f"[bold cyan]Kota: [bold green]{city}\n"
-        f"[bold cyan]Timezone: [bold green]{times}"width=70, padding=(0, 7), style=f"{color_panel}"))
+	print(nel("[green]Author: [purple]Khoirul-Xd\n
+        "[green]Status: [red]Premium\n
+        "[green]GitHub: [blue]https://github.com/khoirulez\n",width=70, padding=(0, 7), style=f"{color_panel}"))
+    print(nel(f"[bold cyan]Your IP: [bold green]{cek_data}\n
+        f"[bold cyan]Region: [bold green]{asal_reg}\n
+        f"[bold cyan]Kota: [bold green]{city}\n
+        f"[bold cyan]Timezone: [bold green]{times}",width=70, padding=(0, 7), style=f"{color_panel}"))
     console.print(Panel(ip_info, width=70, padding=(0, 7), style=f"{color_panel}"))
 	prints(nel(f'              {P2}Menu Tools Crack{P2}',width=70,padding=(0,7),style=f"{color_panel}")) 
 	prints(nel(f"""{P2}[{color_text}01{P2}] Crack Massal    [{color_text}04{P2}] Cek Ressult
