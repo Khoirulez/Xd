121fe1f3d9e831b4be78ed3919ed4f407ea7ee40
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-02 17:13:03 +0700

    Add files via upload

--- b-fb.py
+++ b-fb.py
@@ -373,7 +373,7 @@ def login():
              [bold green]Status: [red]Premium
              [bold green]WhatsApp: [blue]081283547452
              [bold green]GitHub: [blue]https://github.com/khoirulez""",width=70, padding=(0, 7), style=f"{color_panel}"))
-	print(nel(" "dapatkan_info_lokasi()))
+	print(nel("dapatkan_info_lokasi()"))
 	prints(nel(f'              {P2}[bold blue]Menu Tools Crack{P2}',width=70,padding=(0,7),style=f"{color_panel}")) 
 	prints(nel(f"""{P2}{color_text}01{P2} [bold green]Crack Massal    {color_text}04{P2} [bold green]Cek Ressult
 {color_text}02{P2} [bold green]Crack Publik    {color_text}05{P2} [bold green]Crack File
