7d9cec67fcf0f64fa03b8836991bb80021cf7a9d
Khoirulez <123630353+Khoirulez@users.noreply.github.com>
2024-02-02 16:57:07 +0700

    Add files via upload

--- b-fb.py
+++ b-fb.py
@@ -212,11 +212,11 @@ def dapatkan_info_lokasi():
         data = response.json()
 
         # Menampilkan informasi yang diperoleh
-        print(f"{H}Alamat IP{H}: {data['ip']}")
-        print(f"{H}Region{H}: {data['region']}")
-        print(f"{H}Lokasi{H}: {data['loc']}")
-        print(f"{H}Kota{H}: {data['city']}")
-        print(f"{H}Timezone{H}: {data['timezone']}")
+        print(f"{H2}Alamat IP{H2}: {data['ip']}")
+        print(f"{H2}Region{H2}: {data['region']}")
+        print(f"{H2}Lokasi{H2}: {data['loc']}")
+        print(f"{H2}Kota{H2}: {data['city']}")
+        print(f"{H2}Timezone{H2}: {data['timezone']}")
 
     except Exception as e:
         print(f"Error: {e}")
@@ -228,7 +228,6 @@ lebar_layar = 80
 
 # Menghitung jumlah spasi di awal untuk menengahkan pesan
 spasi_awal = (lebar_layar - len(info_user)) // 2
-
 #--------------------[ CONVERTER-BULAN ]--------------#
 dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
 dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
@@ -376,7 +375,7 @@ def login():
              [bold green]Status: [red]Premium
              [bold green]WhatsApp: [blue]081283547452
              [bold green]GitHub: [blue]https://github.com/khoirulez""",width=70, padding=(0, 7), style=f"{color_panel}"))
-	dapatkan_info_lokasi()
+	print(nel(" "* spasi_awal+info_user))
 	prints(nel(f'              {P2}[bold blue]Menu Tools Crack{P2}',width=70,padding=(0,7),style=f"{color_panel}")) 
 	prints(nel(f"""{P2}{color_text}01{P2} [bold green]Crack Massal    {color_text}04{P2} [bold green]Cek Ressult
 {color_text}02{P2} [bold green]Crack Publik    {color_text}05{P2} [bold green]Crack File
@@ -776,8 +775,8 @@ def passwrd():
 				pool.submit(metod3,idf,pwv)
 			else:
 				pool.submit(crackmbasic,idf,pwv)
-	print(f'[{b}•{x}]{h} OK : {h}%s '%(ok))
-	print(f'{x}[{b}•{x}]{k} CP : {k}%s{x} '%(cp))
+	print(f'[{b}•{x}]{h2} OK : {h2}%s '%(ok))
+	print(f'{x}[{b}•{x}]{k2} CP : {k2}%s{x} '%(cp))
 #--------------------[ METODE VALIDATE ]-----------------#
 def validate1(idf,pwv):
 	global loop,ok,cp
